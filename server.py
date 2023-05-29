b5537ba9680ed9528ae2fb1aeda2401df3442654
ophx <castytastyhappybat@gmail.com>
2023-05-28 20:36:33 -0400

    first commnit

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from markupsafe import escape
+from datetime import datetime, timezone 
+
+app = Flask(__name__)
+
+# Home page
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+# Me
+@app.route("/users/@me")
+def me():
+    return render_template('me.html')
+
+# Other users
+@app.route("/users/<int:snowflake>")
+def snowflake(snowflake):
+    timstamp = int(datetime.now(tz=timezone.utc).timestamp() * 1000)
+    return f"Snowflake: {timstamp}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
